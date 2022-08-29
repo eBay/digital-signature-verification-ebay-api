3b44f71552ff9f8ad5aa5411647159bee7dfe325
Ulrich Herberg <uherberg@ebay.com>
2022-08-29 13:53:41 -0700

    added note

--- src/main/java/com/ebay/signaturevalidation/SignatureService.java
+++ src/main/java/com/ebay/signaturevalidation/SignatureService.java
@@ -178,7 +178,7 @@ public class SignatureService {
             }
 
             signatureInputBuf.append(");created=");
-            signatureInputBuf.append("1658440308");
+            signatureInputBuf.append("1658440308"); // TODO: This makes testing easier. But for real production code, you will want to use the next line instead of this one
 //            signatureInputBuf.append(Instant.now().getEpochSecond());
             signatureInput = signatureInputBuf.toString();
 
